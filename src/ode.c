d5fe6cc432cc40be4c2850e51417a38a880542a2
Kevin Müller <mkevin2006@gmail.com>
2024-05-03 20:40:20 +0200

    Construct basis for ODE approximation

--- src/ode.c
+++ src/ode.c
@@ -1,1 +1,16 @@
 #include "ode.h"
+
+double* get_function_array(int width, double step, unsigned int subdivision) {
+    double* func_array = malloc(sizeof(double) * width);
+
+    // Plot
+    for (int i = 0; i < width; i++) {
+
+        // Iterate function
+        for (int j = 0; j < subdivision; j++) {
+
+        }
+    }
+
+    return func_array
+}
