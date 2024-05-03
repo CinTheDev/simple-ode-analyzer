ee205b9bb08708a1fb4efd40de470fe08e9c5c50
Kevin Müller <mkevin2006@gmail.com>
2024-05-03 19:57:15 +0200

    Restructure plot drawing

--- src/plotter.c
+++ src/plotter.c
@@ -2,42 +2,66 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <math.h>
 
 bool compare_doubles(double a, double b, double epsilon) {
     double val = abs(a - b);
     return val < epsilon;
 }
 
-void plot_function(uint16_t width, uint16_t height, double step_x, double step_y, double (*function)(double)) {
-    uint16_t x_axis_index = height + 1;
-    uint16_t y_axis_index = 0;
-
-    for (uint16_t y = 0; y < height * 2 + 1; y++) {
-        for (uint16_t x = 0; x < width * 2 + 1; x++) {
-
-            double coord_x = x * step_x;
-            double coord_y = (y - height - 1) * -step_y;
-
-            double epsilon = step_y;
-
-            double func_val = function(coord_x);
-            if (compare_doubles(func_val, coord_y, epsilon)) {
-                printf("*");
-            }
-            else if (x == y_axis_index && y == x_axis_index) {
-                printf("+");
-            } 
-            else if (x == y_axis_index) {
-                printf("|");
-            }
-            else if (y == x_axis_index) {
-                printf("-");
-            }
-            else {
-                printf(" ");
-            }
+void print_graph(char* graph, uint16_t width, uint16_t height) {
+    for (int y = 0; y < height * 2 + 1; y++) {
+        for (int x = 0; x < width * 2 + 1; x++) {
+            int graph_index = (y * (width * 2 + 1)) + x;
+            printf("%c", graph[graph_index]);
         }
 
         printf("\n");
     }
+}
+
+void draw_graph(char* graph, uint16_t width, uint16_t height) {
+    uint16_t x_axis_index = height;
+    uint16_t y_axis_index = 0;
+
+    for (uint16_t y = 0; y < height * 2 + 1; y++) {
+        for (uint16_t x = 0; x < width * 2 + 1; x++) {
+            int graph_index = (y * (width * 2 + 1)) + x;
+
+            if (x == y_axis_index && y == x_axis_index) {
+                graph[graph_index] = '+';
+            } 
+            else if (x == y_axis_index) {
+                graph[graph_index] = '|';
+            }
+            else if (y == x_axis_index) {
+                graph[graph_index] = '-';
+            }
+            else {
+                graph[graph_index] = ' ';
+            }
+        }
+    }
+}
+
+void plot_function(uint16_t width, uint16_t height, double step_x, double step_y, double (*function)(double)) {
+    int graph_length = (width * 2 + 1) * (height * 2 + 1);
+    char graph[graph_length];
+
+    draw_graph(graph, width, height);
+
+    for (uint16_t x = 0; x < width * 2 + 1; x++) {
+        double coord_x = x * step_x;
+
+        double func_val = function(coord_x);
+
+        int index_y = round(func_val / -step_y + height);
+        
+        if (index_y > 0 && index_y < height * 2) {
+            int graph_index = (index_y * (width * 2 + 1)) + x;
+            graph[graph_index] = '*';
+        }
+    }
+
+    print_graph(graph, width, height);
 }
