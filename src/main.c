e03a09d7dfd9df2ac53a7cda9a085ee850dedec7
Kevin Müller <mkevin2006@gmail.com>
2024-05-03 20:01:44 +0200

    Test out sin drawing

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <math.h>
 #include "plotter.h"
 
 double test_function(double val) {
-    return val * 0.5;
+    return sin(val);
 }
 
 int main() {
-    plot_function(40, 5, 0.25, 1.0, test_function);
+    plot_function(40, 5, 0.25, 0.25, test_function);
 
     return 0;
 }
