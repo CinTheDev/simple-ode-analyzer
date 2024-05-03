c1fbe4c1e3513b604952274740cac1c541353158
Kevin Müller <mkevin2006@gmail.com>
2024-05-03 19:23:32 +0200

    Implement the function plotter

--- include/diffeq/plotter.h
+++ include/diffeq/plotter.h
@@ -3,6 +3,6 @@
 
 #include <stdint.h>
 
-void plot_function(uint16_t width, uint16_t height, uint16_t step, double (*function)(double));
+void plot_function(uint16_t width, uint16_t height, double step, double (*function)(double));
 
 #endif
--- src/plotter.c
+++ src/plotter.c
@@ -1,7 +1,40 @@
 #include "plotter.h"
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-void plot_function(uint16_t width, uint16_t height, uint16_t step, double (*function)(double)) {
-    double test = function(0.0);
-    printf("Test value: %lf\n", test);
+bool compare_doubles(double a, double b, double epsilon) {
+    double val = abs(a - b);
+    return val < epsilon;
+}
+
+void plot_function(uint16_t width, uint16_t height, double step, double (*function)(double)) {
+    uint16_t x_axis_index = height + 1;
+    uint16_t y_axis_index = 0;
+
+    for (uint16_t y = 0; y < height * 2 + 1; y++) {
+        for (uint16_t x = 0; x < width * 2 + 1; x++) {
+
+            double coord_x = x * step;
+            double coord_y = (y - height - 1) * step;
+
+            double epsilon = step;
+
+            double func_val = function(coord_x);
+            if (compare_doubles(func_val, coord_y, epsilon)) {
+                printf("*");
+                goto CONTINUE_SKIP;
+            } 
+
+            if (x == y_axis_index) {
+                printf("|");
+            }
+        }
+
+        if (y == x_axis_index) {
+            printf("-");
+        }
+
+        CONTINUE_SKIP:
+    }
 }
