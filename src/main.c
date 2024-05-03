a4a1c18e8bd46997762cb509dfa00f1e7a63847a
Kevin Müller <mkevin2006@gmail.com>
2024-05-03 22:09:06 +0200

    Account for speed accumulation

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@ double* test_function() {
 
 int main() {
     double* func_values = get_function_array(80, 0.5, 1);
-    plot_function(80, 5, 0.5, 0.5, func_values);
+    plot_function(80, 5, 0.5, 0.25, func_values);
     free(func_values);
 
     return 0;
--- src/ode.c
+++ src/ode.c
@@ -6,11 +6,12 @@ double* get_function_array(int width, double step, unsigned int subdivision) {
     double* func_array = malloc(sizeof(double) * width);
 
     double current_s = 1.0;
+    double current_ds = 0.0;
 
     double dt = step / (double)subdivision;
 
-    double D = 2.0;
-    double m = 1.0;
+    double D = 1.0;
+    double m = 5.0;
 
     // Plot
     for (int i = 0; i < width; i++) {
@@ -19,9 +20,9 @@ double* get_function_array(int width, double step, unsigned int subdivision) {
         // Iterate function
         for (int j = 0; j < subdivision; j++) {
             double dds = -(D / m) * current_s;
-            double ds = dds * dt;
+            current_ds += dds * dt;
 
-            current_s += ds * dt;
+            current_s += current_ds * dt;
         }
     }
 
