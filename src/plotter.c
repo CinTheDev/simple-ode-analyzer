22dea28ef73a6a9099b8bee75b790586a5a408cb
Kevin Müller <mkevin2006@gmail.com>
2024-05-03 20:03:50 +0200

    Make x axis scaling consistent

--- src/plotter.c
+++ src/plotter.c
@@ -11,8 +11,8 @@ bool compare_doubles(double a, double b, double epsilon) {
 
 void print_graph(char* graph, uint16_t width, uint16_t height) {
     for (int y = 0; y < height * 2 + 1; y++) {
-        for (int x = 0; x < width * 2 + 1; x++) {
-            int graph_index = (y * (width * 2 + 1)) + x;
+        for (int x = 0; x < width; x++) {
+            int graph_index = (y * width) + x;
             printf("%c", graph[graph_index]);
         }
 
@@ -25,8 +25,8 @@ void draw_graph(char* graph, uint16_t width, uint16_t height) {
     uint16_t y_axis_index = 0;
 
     for (uint16_t y = 0; y < height * 2 + 1; y++) {
-        for (uint16_t x = 0; x < width * 2 + 1; x++) {
-            int graph_index = (y * (width * 2 + 1)) + x;
+        for (uint16_t x = 0; x < width; x++) {
+            int graph_index = (y * width) + x;
 
             if (x == y_axis_index && y == x_axis_index) {
                 graph[graph_index] = '+';
@@ -45,7 +45,7 @@ void draw_graph(char* graph, uint16_t width, uint16_t height) {
 }
 
 void draw_function(char* graph, uint16_t width, uint16_t height, double step_x, double step_y, double (*function)(double)) {
-    for (uint16_t x = 0; x < width * 2 + 1; x++) {
+    for (uint16_t x = 0; x < width; x++) {
         double coord_x = x * step_x;
 
         double func_val = function(coord_x);
@@ -53,14 +53,14 @@ void draw_function(char* graph, uint16_t width, uint16_t height, double step_x, 
         int index_y = round(func_val / -step_y + height);
 
         if (index_y > 0 && index_y < height * 2) {
-            int graph_index = (index_y * (width * 2 + 1)) + x;
+            int graph_index = (index_y * width) + x;
             graph[graph_index] = '*';
         }
     }
 }
 
 void plot_function(uint16_t width, uint16_t height, double step_x, double step_y, double (*function)(double)) {
-    int graph_length = (width * 2 + 1) * (height * 2 + 1);
+    int graph_length = width * (height * 2 + 1);
     char graph[graph_length];
 
     draw_graph(graph, width, height);
