fe0a558a8ba6e93563ff62642cd7fb837244be53
Kevin Müller <mkevin2006@gmail.com>
2024-05-04 17:42:51 +0200

    Implement layout instancing

--- app/main.cpp
+++ app/main.cpp
@@ -10,5 +10,13 @@ Diffeq_Main::~Diffeq_Main() {
 }
 
 void Diffeq_Main::instance_layout() {
+    plotter = new Plotter(this);
+    controls = new Controls(this);
 
+    sizer_main = new wxBoxSizer(wxVERTICAL);
+
+    sizer_main->Add(plotter, 3, wxEXPAND);
+    sizer_main->Add(controls, 1, wxEXPAND);
+
+    SetSizer(sizer_main);
 }
