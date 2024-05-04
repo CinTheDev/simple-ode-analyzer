92a62ffe5644dcc3a70e4fdeb1eeb74b9289106a
Kevin Müller <mkevin2006@gmail.com>
2024-05-04 17:40:39 +0200

    Define elements in main for plotter and controls

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,14 @@
 #include "main.h"
 
-Diffeq_Main::Diffeq_Main() : wxFrame(nullptr, wxID_ANY, "Differential Equation analyzer", wxDefaultPosition, wxSize(1280, 720)) {
-
+Diffeq_Main::Diffeq_Main() {
+    wxFrame(nullptr, wxID_ANY, "Differential Equation analyzer", wxDefaultPosition, wxSize(1280, 720));
+    instance_layout();
 }
 
 Diffeq_Main::~Diffeq_Main() {
-    
+
+}
+
+void Diffeq_Main::instance_layout() {
+
 }
--- include/diffeq/app/main.h
+++ include/diffeq/app/main.h
@@ -2,11 +2,22 @@
 #define DIFFEQ_MAIN
 
 #include <wx/wx.h>
+#include "plotter.h"
+#include "controls.h"
 
 class Diffeq_Main : public wxFrame {
 public:
     Diffeq_Main();
     ~Diffeq_Main();
+
+public:
+    wxBoxSizer* sizer_main;
+
+    Plotter* plotter;
+    Controls* controls;
+
+private:
+    void instance_layout();
 };
 
 #endif
