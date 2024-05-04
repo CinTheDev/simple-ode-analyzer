2c6c50f18389447ca250a1254da0f79a7451af64
Kevin Müller <mkevin2006@gmail.com>
2024-05-04 17:35:07 +0200

    Define simple structure for controls

--- include/diffeq/controls/controls.h
+++ include/diffeq/controls/controls.h
@@ -1,6 +1,10 @@
 #ifndef DIFFEQ_CONTROLS
 #define DIFFEQ_CONTROLS
 
-
+class Controls : public wxPanel {
+public:
+    Controls(wxWindow* parent);
+    ~Controls();
+};
 
 #endif
--- src/controls/controls.cpp
+++ src/controls/controls.cpp
@@ -1,1 +1,9 @@
 #include "controls.h"
+
+Controls::Controls(wxWindow* parent) {
+    wxPanel(parent, wxID_ANY);
+}
+
+Controls::~Controls() {
+    
+}
