81e031eac9ba2272339d79043af65d8f5e9a2f05
Kevin Müller <mkevin2006@gmail.com>
2024-05-04 17:02:37 +0200

    Restructure main

---
+++ app/main.cpp
@@ -0,0 +1,9 @@
+#include "main.h"
+
+Diffeq_Main::Diffeq_Main() : wxFrame(nullptr, wxID_ANY, "Differential Equation analyzer", wxDefaultPosition, wxSize(1280, 720)) {
+
+}
+
+Diffeq_Main::~Diffeq_Main() {
+    
+}
---
+++ include/diffeq/app/main.h
@@ -0,0 +1,12 @@
+#ifndef DIFFEQ_MAIN
+#define DIFFEQ_MAIN
+
+#include <wx/wx.h>
+
+class Diffeq_Main : public wxFrame {
+public:
+    Diffeq_Main();
+    ~Diffeq_Main();
+};
+
+#endif
