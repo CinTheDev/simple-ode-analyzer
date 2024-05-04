0e6d68f008b7777d66d1aef60767feca6a53f2f7
Kevin Müller <mkevin2006@gmail.com>
2024-05-04 18:33:29 +0200

    Draw black background

--- src/plotter/plotter.cpp
+++ src/plotter/plotter.cpp
@@ -25,16 +25,6 @@ void Plotter::paintNow() {
 }
 
 void Plotter::render(wxDC& dc) {
-    dc.DrawText(wxT("Testing"), 40, 60);
-
-    dc.SetBrush(*wxGREEN_BRUSH);
-    dc.SetPen(wxPen(wxColor(255,0,0), 5));
-    dc.DrawCircle(wxPoint(200,100), 25);
-    
-    dc.SetBrush(*wxBLUE_BRUSH);
-    dc.SetPen(wxPen(wxColor(255,175,175), 10));
-    dc.DrawRectangle(300, 100, 400, 200);
-    
-    dc.SetPen(wxPen(wxColor(0,0,0), 3));
-    dc.DrawLine(300, 100, 700, 300);
+    dc.SetBackground(*wxBLACK_BRUSH);
+    dc.Clear();
 }
