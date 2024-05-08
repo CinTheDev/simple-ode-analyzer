35310d3b48a082cfcbf80a0fd52249b7bcfaa8b3
Kevin Müller <mkevin2006@gmail.com>
2024-05-08 18:29:09 +0200

    Add struct for plotter view settings

--- include/diffeq/controls/controls.h
+++ include/diffeq/controls/controls.h
@@ -3,6 +3,12 @@
 
 #include <wx/wx.h>
 
+struct Settings_Plotter {
+public:
+    double step_x;
+    double step_y;
+};
+
 class Controls : public wxPanel {
 public:
     Controls(wxWindow* parent);
@@ -12,6 +18,12 @@ public:
     wxButton* test_button;
 
     wxBoxSizer* sizer_main;
+
+private:
+    Settings_Plotter settings_plotter;
+
+private:
+    void init_settings_plotter();
 };
 
 #endif
--- src/controls/controls.cpp
+++ src/controls/controls.cpp
@@ -6,8 +6,16 @@ Controls::Controls(wxWindow* parent) : wxPanel(parent, wxID_ANY) {
     sizer_main = new wxBoxSizer(wxHORIZONTAL);
     sizer_main->Add(test_button, 1, wxEXPAND);
     SetSizer(sizer_main);
+
+    init_settings_plotter();
 }
 
 Controls::~Controls() {
 
+}
+
+void Controls::init_settings_plotter() {
+    settings_plotter = Settings_Plotter();
+    settings_plotter.step_x = 1.0;
+    settings_plotter.step_y = 1.0;
 }
