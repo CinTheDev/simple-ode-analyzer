78ac6d92eed8debe971d974d1dc4793767343598
Kevin Müller <mkevin2006@gmail.com>
2024-05-08 20:14:04 +0200

    Remove old code

--- include/diffeq/controls/controls.h
+++ include/diffeq/controls/controls.h
@@ -12,13 +12,10 @@ public:
     Settings_Plotter get_settings_plotter();
 
 public:
-    //wxButton* test_button;
     wxTextCtrl* input_step_x;
     wxTextCtrl* input_step_y;
 
     wxBoxSizer* sizer_main;
-
-    DECLARE_EVENT_TABLE();
 
 private:
     Settings_Plotter settings_plotter;
@@ -27,7 +24,6 @@ private:
     void init_elements();
     void init_settings_plotter();
     void update_settings_plotter();
-    //void on_button_test(wxCommandEvent& evt);
     void on_controls_plotter_changed(wxEvent& evt);
 };
 
--- src/controls/controls.cpp
+++ src/controls/controls.cpp
@@ -1,22 +1,11 @@
 #include "controls.h"
 #include "events.h"
-#include "stdio.h"
-
-wxBEGIN_EVENT_TABLE(Controls, wxPanel)
-
-    //EVT_BUTTON(11000, Controls::on_button_test)
-    //EVT_TEXT_ENTER(11000, Controls::on_controls_plotter_changed)
-    //EVT_TEXT_ENTER(11001, Controls::on_controls_plotter_changed)
-
-wxEND_EVENT_TABLE()
 
 Controls::Controls(wxWindow* parent) : wxPanel(parent, wxID_ANY) {
-    //test_button = new wxButton(this, 11000, "Controls");
     init_settings_plotter();
     init_elements();
 
     sizer_main = new wxBoxSizer(wxVERTICAL);
-    //sizer_main->Add(test_button, 1, wxEXPAND);
     sizer_main->Add(input_step_x, wxEXPAND);
     sizer_main->Add(input_step_y, wxEXPAND);
     SetSizer(sizer_main);
@@ -40,16 +29,7 @@ Settings_Plotter Controls::get_settings_plotter() {
 void Controls::init_settings_plotter() {
     settings_plotter = Settings_Plotter();
 }
-
-/*
-void Controls::on_button_test(wxCommandEvent& evt) {
-    // Test event
-    SettingsPlotterEvent settings_plotter_event = SettingsPlotterEvent(SETTINGS_PLOTTER_UPDATE, GetId(), settings_plotter);
-    settings_plotter_event.SetEventObject(this);
-    ProcessEvent(settings_plotter_event);
-}
-*/
-
+    printf("Event generated\n");
 void Controls::update_settings_plotter() {
     try { settings_plotter.step_x = std::stod(input_step_x->GetValue().ToStdString()); }
     catch (...) {
@@ -68,5 +48,4 @@ void Controls::on_controls_plotter_changed(wxEvent& evt) {
     SettingsPlotterEvent settings_plotter_event = SettingsPlotterEvent(SETTINGS_PLOTTER_UPDATE, GetId(), settings_plotter);
     settings_plotter_event.SetEventObject(this);
     ProcessEvent(settings_plotter_event);
-    printf("Event generated\n");
 }
