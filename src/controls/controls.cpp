188864e030cfbe1a6e3854333376534e2ae97500
Kevin Müller <mkevin2006@gmail.com>
2024-05-08 18:55:39 +0200

    Send out test event

--- src/controls/controls.cpp
+++ src/controls/controls.cpp
@@ -1,5 +1,5 @@
 #include "controls.h"
-#include "event.h"
+#include "events.h"
 
 Controls::Controls(wxWindow* parent) : wxPanel(parent, wxID_ANY) {
     test_button = new wxButton(this, wxID_ANY, "Controls");
@@ -9,6 +9,11 @@ Controls::Controls(wxWindow* parent) : wxPanel(parent, wxID_ANY) {
     SetSizer(sizer_main);
 
     init_settings_plotter();
+
+    // Test event
+    SettingsPlotterEvent settings_plotter_event = SettingsPlotterEvent(SETTINGS_PLOTTER_UPDATE, GetId());
+    settings_plotter_event.SetEventObject(this);
+    ProcessEvent(settings_plotter_event);
 }
 
 Controls::~Controls() {
