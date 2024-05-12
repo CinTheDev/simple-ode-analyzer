595dcd09050b26e2b11f704aad136f9d425d5258
Kevin Müller <mkevin2006@gmail.com>
2024-05-12 18:29:44 +0200

    Put label construction into temporary pointer

--- src/controls/view/controls_view.cpp
+++ src/controls/view/controls_view.cpp
@@ -16,9 +16,14 @@ void ControlsView::init_sizers() {
     sizer_main = new wxFlexGridSizer(2, 5, 5);
     sizer_main->AddGrowableCol(0, 1);
 
-    sizer_main->Add(new wxStaticText(this, wxID_ANY, "Visible X space"));
+    wxStaticText* temp_ptr_label;
+    
+    temp_ptr_label = new wxStaticText(this, wxID_ANY, "Visible X space");
+    sizer_main->Add(temp_ptr_label);
     sizer_main->Add(input_view_x);
-    sizer_main->Add(new wxStaticText(this, wxID_ANY, "Visible Y space"));
+
+    temp_ptr_label = new wxStaticText(this, wxID_ANY, "Visible Y space");
+    sizer_main->Add(temp_ptr_label);
     sizer_main->Add(input_view_y);
 
     SetSizer(sizer_main);
