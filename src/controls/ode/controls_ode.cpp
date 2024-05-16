42e1fb590b614584f03515d668e9b091b75b2f23
Kevin Müller <mkevin2006@gmail.com>
2024-05-16 20:10:25 +0200

    Send event for common settings with new result as well

--- src/controls/ode/controls_ode.cpp
+++ src/controls/ode/controls_ode.cpp
@@ -23,15 +23,25 @@ ControlsODE::~ControlsODE() {
 }
 
 void ControlsODE::SendResults() {
-    OdePointerEvent event(EVT_ODE_POINTER, GetId(), ode->get_result(), ode->get_length());
-    event.SetEventObject(this);
-    event.ResumePropagation(__INT_MAX__);
-    ProcessEvent(event);
+    // ODE pointer
+    OdePointerEvent evt_ode_pointer(EVT_ODE_POINTER, GetId(), ode->get_result(), ode->get_length());
+    evt_ode_pointer.SetEventObject(this);
+    evt_ode_pointer.ResumePropagation(__INT_MAX__);
+    ProcessEvent(evt_ode_pointer);
+
+    // Step x
+    Settings_Common settings_common;
+    construct_common_settings(&settings_common);
+
+    SettingsCommonEvent evt_settings_common(SETTINGS_COMMON_UPDATE, GetId(), settings_common);
+    evt_settings_common.SetEventObject(this);
+    evt_settings_common.ResumePropagation(__INT_MAX__);
+    ProcessEvent(evt_settings_common);
 }
 
 void ControlsODE::construct_common_settings(Settings_Common* settings) {
     // TODO: Implement custom values
-    settings->step_x = 0.01;
+    settings->step_x = 0.1;
 }
 
 void ControlsODE::on_test_button(wxCommandEvent& evt) {
