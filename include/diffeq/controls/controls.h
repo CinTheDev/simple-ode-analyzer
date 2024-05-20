e10c94d81d600713a9f24217684f9ffeb32a9d76
Kevin Müller <mkevin2006@gmail.com>
2024-05-20 16:16:49 +0200

    Implement class Controls

--- include/diffeq/controls/controls.h
+++ include/diffeq/controls/controls.h
@@ -4,7 +4,21 @@
 #include <wx/wx.h>
 
 class Controls : public wxScrolledWindow {
+public:
+    Controls(wxWindow* parent, wxString label);
+    ~Controls();
 
+    wxStaticBoxSizer* sizer_main;
+    wxFlexGridSizer* sizer_grid;
+
+public:
+    void on_text_input(wxEvent& evt);
+
+private:
+    double get_input_double(wxTextCtrl* input_field, wxStaticText* label);
+    int get_input_int(wxTextCtrl* input_field, wxStaticText* label);
+
+    wxString double_to_string(double val);
 };
 
 #endif
--- src/controls/controls.cpp
+++ src/controls/controls.cpp
@@ -1,1 +1,59 @@
 #include "controls.h"
+
+Controls::Controls(wxWindow* parent, wxString label) : wxScrolledWindow(parent) {
+    Bind(wxEVT_TEXT, &Controls::on_text_input, this);
+
+    sizer_grid = new wxFlexGridSizer(2, 5, 5);
+    sizer_grid->AddGrowableCol(0, 1);
+
+    sizer_main = new wxStaticBoxSizer(wxVERTICAL, this, label);
+    sizer_main->Add(sizer_grid, 1, wxEXPAND | wxALL, 10);
+
+    SetSizer(sizer_main);
+
+    FitInside();
+    SetScrollRate(5, 5);
+}
+
+Controls::~Controls() { }
+
+void Controls::on_text_input(wxEvent& evt) {
+    // TODO: Generic implementation
+}
+
+double Controls::get_input_double(wxTextCtrl* input_field, wxStaticText* label) {
+    double val;
+    try {
+        val = std::stod(input_field->GetValue().ToStdString());
+        label->SetForegroundColour(*wxWHITE);
+    }
+    catch (...) {
+        val = 1.0;
+        label->SetForegroundColour(*wxRED);
+    }
+
+    return val;
+}
+
+int Controls::get_input_int(wxTextCtrl* input_field, wxStaticText* label) {
+    int val;
+
+    try {
+        val = std::stoi(input_field->GetValue().ToStdString());
+        label->SetForegroundColour(*wxWHITE);
+    }
+    catch (...) {
+        val = 0;
+        label->SetForegroundColour(*wxRED);
+    }
+
+    return val;
+}
+
+wxString Controls::double_to_string(double val) {
+    std::string string_val = std::to_string(val);
+    string_val.erase(string_val.find_last_not_of('0') + 1, std::string::npos);
+    string_val.erase(string_val.find_last_not_of('.') + 1, std::string::npos);
+
+    return wxString(string_val);
+}
