9870c281a0b52ff1810dd088c20a81f6bb64d977
Kevin Müller <mkevin2006@gmail.com>
2024-05-20 21:39:17 +0200

    Define new class for choosing ODEs

---
+++ include/diffeq/controls/ode/ode_chooser/controls_choose.h
@@ -0,0 +1,13 @@
+#ifndef DIFFEQ_CONTROLS_CHOOSE
+#define DIFFEQ_CONTROLS_CHOOSE
+
+#include <wx/wx.h>
+#include "controls.h"
+
+class ControlsChoose : public Controls {
+public:
+    ControlsChoose(wxWindow* parent);
+    ~ControlsChoose();
+};
+
+#endif
