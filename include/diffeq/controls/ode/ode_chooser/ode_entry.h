c9deabd4e501f120ba9252093110895b07377115
Kevin Müller <mkevin2006@gmail.com>
2024-05-21 13:42:04 +0200

    Define new class for ode entries

---
+++ include/diffeq/controls/ode/ode_chooser/ode_entry.h
@@ -0,0 +1,12 @@
+#ifndef DIFFEQ_ODE_ENTRY
+#define DIFFEQ_ODE_ENTRY
+
+#include <wx/wx.h>
+
+class OdeEntry : public wxPanel {
+public:
+    OdeEntry(wxWindow* parent);
+    ~OdeEntry();
+};
+
+#endif
