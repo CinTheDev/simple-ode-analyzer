4f7776f81c75d73a58bb1b4c324ae806a68c3648
Kevin Müller <mkevin2006@gmail.com>
2024-05-21 14:11:45 +0200

    Add Layout TODO in ode_entry.h

--- include/diffeq/controls/ode/ode_chooser/ode_entry.h
+++ include/diffeq/controls/ode/ode_chooser/ode_entry.h
@@ -3,6 +3,9 @@
 
 #include <wx/wx.h>
 
+// TODO: Layout
+// | ODE Type (Dropdown) | Numerical Method (Dropdown) | Color (Color picker) | Remove button |
+
 class OdeEntry : public wxPanel {
 public:
     OdeEntry(wxWindow* parent);
