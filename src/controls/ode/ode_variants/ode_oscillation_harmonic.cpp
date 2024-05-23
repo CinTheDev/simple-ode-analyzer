7f109fbcd04c71a90978cd415082b87122418376
Kevin Müller <mkevin2006@gmail.com>
2024-05-23 23:21:22 +0200

    Add additional options to ode_oscillation_harmonic

--- src/controls/ode/ode_variants/ode_oscillation_harmonic.cpp
+++ src/controls/ode/ode_variants/ode_oscillation_harmonic.cpp
@@ -4,15 +4,19 @@ ODE_Oscillation_Harmonic::ODE_Oscillation_Harmonic() : ODE_Oscillation_Harmonic(
 
 ODE_Oscillation_Harmonic::ODE_Oscillation_Harmonic(Settings_Common settings_common, Settings_Approx settings_approx)
 : ODE(settings_common, settings_approx) {
-    amount_variables = 2;
+    amount_variables = 4;
     variable_values = new double[amount_variables];
     variable_names = new std::string[amount_variables];
 
     variable_names[0] = "D [kg * s^-2]";
     variable_names[1] = "m [kg]";
+    variable_names[2] = "s_0 [m]";
+    variable_names[3] = "d/dt s_0 [m * s^-1]";
 
     variable_values[0] = 100.0;
     variable_values[1] = 1.0;
+    variable_values[2] = 1.0;
+    variable_values[3] = 0.0;
 }
 
 ODE_Oscillation_Harmonic::~ODE_Oscillation_Harmonic() {
@@ -26,8 +30,8 @@ void ODE_Oscillation_Harmonic::calculate() {
     double D = variable_values[0];
     double m = variable_values[1];
 
-    double current_s = 1.0;
-    double current_ds = 0.0;
+    double current_s = variable_values[2];
+    double current_ds = variable_values[3];
 
     for (int i = 0; i < result_length; i++) {
         result[i] = current_s;
