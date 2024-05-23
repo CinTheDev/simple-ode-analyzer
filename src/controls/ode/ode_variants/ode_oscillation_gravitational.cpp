98d508fb402f2c6b5840aacadc8aa5c7305ac564
Kevin Müller <mkevin2006@gmail.com>
2024-05-23 22:54:40 +0200

    Define variables for gravitational oscillation

--- src/controls/ode/ode_variants/ode_oscillation_gravitational.cpp
+++ src/controls/ode/ode_variants/ode_oscillation_gravitational.cpp
@@ -4,15 +4,22 @@
 ODE_Oscillation_Gravitational::ODE_Oscillation_Gravitational() : ODE_Oscillation_Gravitational(Settings_Common(), Settings_Approx()) { }
 
 ODE_Oscillation_Gravitational::ODE_Oscillation_Gravitational(Settings_Common settings_common, Settings_Approx settings_approx)
- : ODE(settings_common, settings_approx) { }
+: ODE(settings_common, settings_approx) {
+    amount_variables = 1;
+    variable_values = new double[amount_variables];
+    variable_names = new std::string[amount_variables];
+
+    variable_names[0] = "a";
+
+    variable_values[0] = 80.0;
+}
 
 ODE_Oscillation_Gravitational::~ODE_Oscillation_Gravitational() { }
 
 void ODE_Oscillation_Gravitational::calculate() {
     double dt = settings_common.step_x / (double)settings_approx.subdivision;
 
-    double g = 80.0;
-    double angle = 90 * (3.141 / 180.0);
+    double a = variable_values[0];
 
     double current_s = 1.0;
     double current_ds = 0.0;
@@ -21,7 +28,7 @@ void ODE_Oscillation_Gravitational::calculate() {
         result[i] = current_s;
 
         for (int j = 0; j < settings_approx.subdivision; j++) {
-            double dds = -g * sin(angle) * (abs(current_s) / current_s);
+            double dds = -a * (abs(current_s) / current_s);
             current_ds += dds * dt;
 
             current_s += current_ds * dt;
