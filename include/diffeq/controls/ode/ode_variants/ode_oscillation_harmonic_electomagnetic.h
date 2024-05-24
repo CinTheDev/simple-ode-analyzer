3757ecc7e93bcd192e907ab2b1909c9f5794b5cb
Kevin Müller <mkevin2006@gmail.com>
2024-05-24 21:13:16 +0200

    Add class for electromagnetic harmonic oscillation

---
+++ include/diffeq/controls/ode/ode_variants/ode_oscillation_harmonic_electomagnetic.h
@@ -0,0 +1,15 @@
+#ifndef ODE_OSCILLATION_HARMONIC_ELECTROMAGNETIC
+#define ODE_OSCILLATION_HARMONIC_ELECTROMAGNETIC
+
+#include "ode_oscillation_harmonic.h"
+
+class ODE_Oscillation_Harmonic_Electromagnetic : public ODE_Oscillation_Harmonic {
+public:
+    ODE_Oscillation_Harmonic_Electromagnetic();
+    ~ODE_Oscillation_Harmonic_Electromagnetic();
+
+protected:
+    OscillationHarmonicVariables read_variables();
+}
+
+#endif
---
+++ src/controls/ode/ode_variants/ode_oscillation_harmonic_electromagnetic.cpp
@@ -0,0 +1,36 @@
+#include "ode_oscillation_harmonic_electomagnetic.h"
+#include <math.h>
+
+ODE_Oscillation_Harmonic_Electromagnetic::ODE_Oscillation_Harmonic_Electromagnetic()
+: ODE() {
+
+    std::string names[] = {
+        "L [H]",
+        "C [F]",
+        "Q_0 [C]",
+        "I_0 [A]",
+    };
+
+    double values[] = {
+        100.0,
+        1.0,
+        1.0,
+        0.0,
+    };
+
+    size_t count = sizeof(values) / sizeof(double);
+
+    init_variables(count, names, values);
+}
+
+ODE_Oscillation_Harmonic_Electromagnetic::~ODE_Oscillation_Harmonic_Electromagnetic() { }
+
+OscillationHarmonicVariables ODE_Oscillation_Harmonic_Electromagnetic::read_variables() {
+    OscillationHarmonicVariables variables = OscillationHarmonicVariables();
+
+    variables.omega = 1.0 / sqrt(variable_values[0] * variable_values[1]);
+    variables.s_0 = variable_values[2];
+    variables.v_0 = variable_values[3];
+
+    return variables;
+}
