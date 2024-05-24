7a43d6da44851ed698a20e87401ef1896b771bb8
Kevin Müller <mkevin2006@gmail.com>
2024-05-24 22:12:25 +0200

    Include new class in CMake build

--- include/diffeq/controls/ode/ode_variants/ode_variants.h
+++ include/diffeq/controls/ode/ode_variants/ode_variants.h
@@ -1,4 +1,6 @@
 #include "ode_oscillation_harmonic.h"
 #include "ode_oscillation_harmonic_mechanical.h"
 #include "ode_oscillation_harmonic_electomagnetic.h"
+
+#include "ode_oscillation_swing.h"
 #include "ode_oscillation_gravitational.h"
