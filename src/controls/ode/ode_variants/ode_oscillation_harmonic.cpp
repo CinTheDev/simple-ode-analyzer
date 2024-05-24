b9226553c701329280bc389f2e7301b8de416950
Kevin Müller <mkevin2006@gmail.com>
2024-05-24 20:02:55 +0200

    Replace variables in harmonic oscillation with more general variable

--- src/controls/ode/ode_variants/ode_oscillation_harmonic.cpp
+++ src/controls/ode/ode_variants/ode_oscillation_harmonic.cpp
@@ -18,15 +18,13 @@ ODE_Oscillation_Harmonic::ODE_Oscillation_Harmonic(Settings_Common settings_comm
 : ODE(settings_common, settings_approx) {
 
     std::string names[] = {
-        "D [kg * s^-2]",
-        "m [kg]",
+        "w [TODO: derive unit]",
         "s_0 [m]",
         "v_0 [m * s^-1]",
     };
 
     double values[] = {
-        100.0,
-        1.0,
+        10.0,
         1.0,
         0.0,
     };
@@ -64,14 +62,12 @@ void ODE_Oscillation_Harmonic::calculate() {
 }
 
 void ODE_Oscillation_Harmonic::calculate_solved() {
-    double D = variable_values[0];
-    double m = variable_values[1];
+    double omega = variable_values[0];
 
-    double s_0 = variable_values[2];
+    double s_0 = variable_values[1];
     // TODO: Do not neglect v_0 in calculation
-    double v_0 = variable_values[3];
+    double v_0 = variable_values[2];
 
-    double omega = sqrt(D / m);
 
     for (size_t i = 0; i < result_length; i++) {
         double t = i * settings_common.step_x;
@@ -82,17 +78,16 @@ void ODE_Oscillation_Harmonic::calculate_solved() {
 void ODE_Oscillation_Harmonic::calculate_euler() {
     double dt = settings_common.step_x / (double)settings_approx.subdivision;
 
-    double D = variable_values[0];
-    double m = variable_values[1];
+    double omega = variable_values[0];
 
-    double current_s = variable_values[2];
-    double current_ds = variable_values[3];
+    double current_s = variable_values[1];
+    double current_ds = variable_values[2];
 
     for (size_t i = 0; i < result_length; i++) {
         result[i] = current_s;
 
         for (size_t j = 0; j < settings_approx.subdivision; j++) {
-            double dds = (-D / m) * current_s;
+            double dds = -omega * omega * current_s;
             current_ds += dds * dt;
 
             current_s += current_ds * dt;
