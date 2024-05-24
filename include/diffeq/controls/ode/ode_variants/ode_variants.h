4e5b30bd18d6ddbec3132c05eb5c2ca770bf5b02
Kevin Müller <mkevin2006@gmail.com>
2024-05-24 21:14:35 +0200

    Include new class to build

--- include/diffeq/controls/ode/ode_variants/ode_variants.h
+++ include/diffeq/controls/ode/ode_variants/ode_variants.h
@@ -1,3 +1,4 @@
 #include "ode_oscillation_harmonic.h"
 #include "ode_oscillation_harmonic_mechanical.h"
+#include "ode_oscillation_harmonic_electomagnetic.h"
 #include "ode_oscillation_gravitational.h"
