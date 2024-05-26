2571627b429a0562160323a524937e1d12f8c248
Kevin Müller <mkevin2006@gmail.com>
2024-05-26 18:33:07 +0200

    Define new class for harmonic oscillation, deriving from OdeEntry

--- include/diffeq/controls/ode/ode_chooser/ode_variants/ode_oscillation_harmonic.h
+++ include/diffeq/controls/ode/ode_chooser/ode_variants/ode_oscillation_harmonic.h
@@ -1,6 +1,15 @@
 #ifndef DIFFEQ_ODE_OSCILLATION_HARMONIC
 #define DIFFEQ_ODE_OSCILLATION_HARMONIC
 
+#include <wx/wx.h>
 
+class Ode_Oscillation_Harmonic : public OdeEntry {
+public:
+    Ode_Oscillation_Harmonic(wxWindow* parent);
+    ~Ode_Oscillation_Harmonic();
+
+    wxStaticText* some_label;
+    wxTextCtrl* some_option;
+};
 
 #endif
--- src/controls/ode/ode_chooser/ode_variants/ode_oscillation_harmonic.cpp
+++ src/controls/ode/ode_chooser/ode_variants/ode_oscillation_harmonic.cpp
@@ -1,1 +1,8 @@
 #include "ode_oscillation_harmonic.h"
+
+Ode_Oscillation_Harmonic::Ode_Oscillation_Harmonic(wxWindow* parent) : OdeEntry(parent, "Harmonic Oscillation") {
+    some_label = new wxStaticText(this, wxID_ANY, "Some label");
+    some_option = new wxTextCtrl(this, wxID_ANY, "Some option");
+}
+
+Ode_Oscillation_Harmonic::~Ode_Oscillation_Harmonic() { }
