242c5768c7ffc417ae7887e0931484bc227d63e3
Kevin Müller <mkevin2006@gmail.com>
2024-05-26 23:05:31 +0200

    Delete now useless events

--- include/diffeq/common/events.h
+++ include/diffeq/common/events.h
@@ -5,42 +5,6 @@
 #include "settings.h"
 #include <cstddef>
 
-class SettingsCommonEvent : public wxEvent {
-public:
-    SettingsCommonEvent(wxEventType event_type, int id, Settings_Common settings_common);
-    ~SettingsCommonEvent();
-
-    virtual wxEvent* Clone() const;
-
-    Settings_Common get_settings();
-
-private:
-    Settings_Common settings_common;
-};
-
-wxDECLARE_EVENT(SETTINGS_COMMON_UPDATE, SettingsCommonEvent);
-
-#define SettingsCommonEventHandler(func) (&func)
-
-class SettingsOdeEvent : public wxEvent {
-public:
-    SettingsOdeEvent(wxEventType event_type, int id, Settings_Common settings_common, Settings_Approx settings_approx);
-    ~SettingsOdeEvent();
-
-    virtual wxEvent* Clone() const;
-
-    Settings_Common get_settings_common();
-    Settings_Approx get_settings_approx();
-
-private:
-    Settings_Common settings_common;
-    Settings_Approx settings_approx;
-};
-
-wxDECLARE_EVENT(SETTINGS_ODE_UPDATE, SettingsOdeEvent);
-
-#define SettingsOdeEventHanlder(func) (&func)
-
 class SettingsPlotterEvent : public wxEvent {
 public:
     SettingsPlotterEvent(wxEventType event_type, int id, Settings_Plotter settings_plotter);
@@ -58,25 +22,6 @@ wxDECLARE_EVENT(SETTINGS_PLOTTER_UPDATE, SettingsPlotterEvent);
 wxDECLARE_EVENT(PLOTTER_GRAPHICS_UPDATE, SettingsPlotterEvent);
 
 #define SettingsPlotterEventHandler(func) (&func)
-
-class SettingsOdeRequest : public wxEvent {
-public:
-    SettingsOdeRequest(wxEventType event_type, int id, Settings_Common* settings_common, Settings_Approx* settings_approx);
-    ~SettingsOdeRequest();
-
-    virtual wxEvent* Clone() const;
-
-    Settings_Common* get_settings_common();
-    Settings_Approx* get_settings_approx();
-
-private:
-    Settings_Common* settings_common;
-    Settings_Approx* settings_approx;
-};
-
-wxDECLARE_EVENT(SETTINGS_ODE_REQUEST, SettingsOdeRequest);
-
-#define SettingsOdeRequestHandler(func) (&func)
 
 class OdePointerEvent : public wxEvent {
 public:
--- src/common/events.cpp
+++ src/common/events.cpp
@@ -1,47 +1,5 @@
 #include <events.h>
 
-// SettingsCommonEvent
-
-wxDEFINE_EVENT(SETTINGS_COMMON_UPDATE, SettingsCommonEvent);
-
-SettingsCommonEvent::SettingsCommonEvent(wxEventType event_type, int id, Settings_Common settings_common) : wxEvent(id, event_type) {
-    this->settings_common = settings_common;
-}
-
-SettingsCommonEvent::~SettingsCommonEvent() { }
-
-wxEvent* SettingsCommonEvent::Clone() const {
-    return new SettingsCommonEvent(*this);
-}
-
-Settings_Common SettingsCommonEvent::get_settings() {
-    return settings_common;
-}
-
-// SettingsOdeEvent
-
-wxDEFINE_EVENT(SETTINGS_ODE_UPDATE, SettingsOdeEvent);
-
-SettingsOdeEvent::SettingsOdeEvent(wxEventType event_type, int id, Settings_Common settings_common, Settings_Approx settings_approx)
-: wxEvent(id, event_type) {
-    this->settings_common = settings_common;
-    this->settings_approx = settings_approx;
-}
-
-SettingsOdeEvent::~SettingsOdeEvent() { }
-
-wxEvent* SettingsOdeEvent::Clone() const {
-    return new SettingsOdeEvent(*this);
-}
-
-Settings_Common SettingsOdeEvent::get_settings_common() {
-    return settings_common;
-}
-
-Settings_Approx SettingsOdeEvent::get_settings_approx() {
-    return settings_approx;
-}
-
 // SettingsPlotterEvent
 
 wxDEFINE_EVENT(SETTINGS_PLOTTER_UPDATE, SettingsPlotterEvent);
@@ -59,30 +17,6 @@ wxEvent* SettingsPlotterEvent::Clone() const {
 
 Settings_Plotter SettingsPlotterEvent::get_settings() {
     return settings_plotter;
-}
-
-// SettingsOdeRequest
-
-wxDEFINE_EVENT(SETTINGS_ODE_REQUEST, SettingsOdeRequest);
-
-SettingsOdeRequest::SettingsOdeRequest(wxEventType event_type, int id, Settings_Common* settings_common, Settings_Approx* settings_approx)
-: wxEvent(id, event_type) {
-    this->settings_common = settings_common;
-    this->settings_approx = settings_approx;
-}
-
-SettingsOdeRequest::~SettingsOdeRequest() { }
-
-wxEvent* SettingsOdeRequest::Clone() const {
-    return new SettingsOdeRequest(*this);
-}
-
-Settings_Common* SettingsOdeRequest::get_settings_common() {
-    return settings_common;
-}
-
-Settings_Approx* SettingsOdeRequest::get_settings_approx() {
-    return settings_approx;
 }
 
 // OdePointerEvent
