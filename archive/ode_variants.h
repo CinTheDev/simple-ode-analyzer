304c7dc8b81b42375cae1ff179f20b65a32961ff
Kevin Müller <mkevin2006@gmail.com>
2024-05-26 22:37:48 +0200

    Delete archive

--- archive/ode_variants.h
+++
@@ -1,6 +0,0 @@
-#include "ode_oscillation_harmonic.h"
-#include "ode_oscillation_harmonic_mechanical.h"
-#include "ode_oscillation_harmonic_electomagnetic.h"
-
-#include "ode_oscillation_swing.h"
-#include "ode_oscillation_gravitational.h"
